822046719edc5e6aa64b2737c75e8884a893b445
Rodrigo Martins <rrmartinsjg@gmail.com>
2016-07-10 11:50:40 -0300

    add functions with Methods HTTP

--- main.go
+++ main.go
@@ -6,11 +6,44 @@ type UserAPI struct {
 	*iris.Context
 }
 
+// Get /users
+func (u UserAPI) Get() {
+	u.Write("Get from /users")
+	// u.JSON(iris.StatusOK, myDb.AllUsers())
+}
+
+// Get /:param1 which its value passed to the id argument
+func (u UserAPI) GetBy(id string) { // id equals to u.Param("param1")
+	u.Write("Get from /users/%s", id)
+	// u.JSON(iris.StatusOK, myDb.GetUserById(id))
+}
+
+// PUT /users
+func (u UserAPI) Put() {
+	name := u.FormValue("name") // you can still use the whole context's feature!
+	// myDb.InsertUser(...)
+	println(string(name))
+	println("Put from /users")
+}
+
+// POST /users/:param1
+func (u UserAPI) PostBy(id string) {
+	name := u.FormValue("name") // you can still use the whole context's feature!
+	// myDb.UpdateUser(...)
+	println(string(name))
+	println("Post from /users/" + id)
+}
+
+// DELETE /users/:param1
+func (u UserAPI) DeleteBy(id string) {
+	// myDb.DeleteUser(id)
+	println("Delete from /" + id)
+}
+
 func main() {
-	// api := iris.New()
-	iris.Config.Render.Template.Engine = iris.PongoEngine
-	iris.Get("/hi", hi)
-	iris.Listen(":8080")
+	api := iris.New()
+	api.API("/users", UserAPI{})
+	api.Listen(":8080")
 }
 
 func hi(ctx *iris.Context) {
